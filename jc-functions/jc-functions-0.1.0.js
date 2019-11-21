f27f1317fb828125438f9080fc47c1f4484a65cd
Christoph <email@ckloth.de>
2019-11-21 22:20:11 +0100

    added tooltip (first try) and icons

--- jc-functions/jc-functions-0.1.0.js
+++ jc-functions/jc-functions-0.1.0.js
@@ -65,5 +65,81 @@ function changeVisibility(id) {
   else               { elementHidden(id); }
   }
 
+//--------------------------------
+// tool tip
+//--------------------------------
+
+function jcTooltip(name) {
+
+	console.log("Start jcTooltip ["+name+"] ...");
+
+	this.appName       = name;
+	this.react_on      = "onmouseover";
+	this.style_tooltip = "";
+	this.style_offset  = ["","",""];
+	this.all_elements  = [];
+	
+	this.settings = function (mode="onmouseover", width="auto", height="auto", offset="") {
+	
+		if (offset != "") { this.style_offset  = [ "bottom:auto;top:"+offset+"px;", "bottom:auto;top:"+(offset-9)+"px;","bottom:auto;top:"+(offset-10)+"px;" ]; }
+		
+		this.style_tooltip = "style=\"height:"+height+";width:"+width+";"+this.style_offset[0]+"\"";
+		this.react_on      = mode;
+		} 
+	
+	this.create   = function (parent_element, tooltip_text, name, left="") {
+		var react_on_cmd = "";
+		var style_tt     = this.style_tooltip;
+		this.all_elements.push(name);
+	
+		if (this.react_on == "onmouseover")  { react_on_cmd = "onmouseover=\""+this.appName+".show('"+name+"');\" onmouseout=\""+this.appName+".hide('"+name+"');\""; }
+		else if (this.react_on == "onclick") { react_on_cmd = "onclick=\""+this.appName+".toggle('"+name+"');\""; }
+		else if (this.react_on == "other")   {}  // external activation
+	
+		var text = "";
+	        text += "<div class='jc_tooltip' "+react_on_cmd+">";
+	        text += parent_element;
+	        text += "<span class='jc_triangle1' id=\"jc_triangle1_" + name + "\" style=\""+this.style_offset[1]+"\"></span>";
+        	text += "<span class='jc_triangle2' id=\"jc_triangle2_" + name + "\" style=\""+this.style_offset[2]+"\"></span>";
+	        text += "<span class=\"jc_tooltiptext " + left + "\" id=\"jc_tooltiptext_" + name + "\" "+style_tt+">" + tooltip_text + "</span>";
+        	text += "</div>";
+		return text;
+		}
+
+	this.show = function (name) {
+	
+	
+		elementVisible("jc_triangle1_" + name);
+		elementVisible("jc_triangle2_" + name);
+		elementVisible("jc_tooltiptext_" + name);
+		}
+		
+	this.hide = function (name) {
+		elementHidden("jc_triangle1_" + name);
+		elementHidden("jc_triangle2_" + name);
+		elementHidden("jc_tooltiptext_" + name);
+		}
+		
+	this.toggle = function (name) {
+		changeVisibility("jc_triangle1_" + name);
+		changeVisibility("jc_triangle2_" + name);
+		changeVisibility("jc_tooltiptext_" + name);
+		//alert(document.getElementById("jc_tooltiptext_" + name).clientHeight);
+		}
+		
+	this.toggleAll = function (name) {
+		changeVisibility("jc_triangle1_" + name);
+		changeVisibility("jc_triangle2_" + name);
+		changeVisibility("jc_tooltiptext_" + name);
+		
+		for (var i=0;i<this.all_elements.length;i++) {
+			if (this.all_elements[i] != name) {
+				elementHidden("jc_triangle1_" + this.all_elements[i]);	
+				elementHidden("jc_triangle2_" + this.all_elements[i]);
+				elementHidden("jc_tooltiptext_" + this.all_elements[i]);		
+			}	}
+		//alert(document.getElementById("jc_tooltiptext_" + name).clientHeight);
+		}
+	}
 
 
